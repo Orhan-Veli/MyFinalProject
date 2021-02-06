c9c45e8e8afb8a18832ba68fdee349d8cd2b670e
Orhan-Veli <orhan_1576@hotmail.com>
2021-02-06 23:25:24 +0300

    Core.

--- DataAcces/Abstract/IOrderDal.cs
+++ DataAcces/Abstract/IOrderDal.cs
@@ -6,8 +6,7 @@ using System.Text;
 
 namespace DataAcces.Abstract
 {
-    public interface IProductDal:IEntityRepository<Product>
+    public interface IOrderDal:IEntityRepository<Order>
     {
-        
     }
 }
